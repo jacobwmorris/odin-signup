a4f518f414f06955b89b98a4dbb1e301bdc66817
jacobwmorris <jacobwmorris@gmail.com>
2022-10-24 16:25:03 -0400

    Add javascript password validation

---
+++ validate.js
@@ -0,0 +1,24 @@
+const pass1 = document.getElementById("password");
+const pass2 = document.getElementById("password2");
+
+function setPassClass(val) {
+    pass1.classList.add(val);
+    pass2.classList.add(val);
+}
+
+function validate() {
+    if (pass1.value === "" || pass2.value === "") {
+        setPassClass("error");
+        return;
+    }
+    if (pass1.value !== pass2.value) {
+        setPassClass("error");
+        return;
+    }
+
+    pass1.classList.remove("error");
+    pass2.classList.remove("error");
+}
+
+pass1.addEventListener("change", validate);
+pass2.addEventListener("change", validate);
